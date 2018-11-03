c83ea64e17222757e6329bc58ab72ccb21319dc6
hazelnutsgz <guozhen.she@intel.com>
2018-11-03 23:33:37 +0800

    Using polling to all the connection to check the result

--- client1.py
+++ client1.py
@@ -2,10 +2,12 @@ import socket
 
 import socket
 HOST = 'localhost'
-PORT = 8003
+PORT = 8006
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
+msg = s.recv(1024)
+print (msg)
 
 while True:
     cmd = input("Please input msg:")
--- server.py
+++ server.py
@@ -1,19 +1,19 @@
 import socket
 
 HOST = 'localhost'
-PORT = 8003
+PORT = 8004
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 s.listen(5)
-s.settimeout(2)
+s.settimeout(0)
 connection_pool = []
 
 while True:
 	for conn in connection_pool:
 		data = ''
 		try:
-			conn.settimeout(1)
+			conn.settimeout(0)
 			temp = conn.recv(1024).decode("utf-8")
 		except Exception:
 			print ("Wrong")
@@ -28,11 +28,10 @@ while True:
 		print (data)
 		conn.send("server received you message.".encode("utf-8"))
 
-	print ("Listen for the new connection....")
 	try:
 	    conn, addr = s.accept()
 	    print ('Connected by ', addr)
-	    conn.send("server received you message.".encode("utf-8"))
+	    conn.send("Thanks for your registration..".encode("utf-8"))
 	    connection_pool.append(conn)
 	except socket.timeout:
 		pass
