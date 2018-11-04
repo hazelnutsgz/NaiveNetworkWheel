eacb5d1990c8eafbe91e4d68ec02a7ab4758d9dd
hazelnutsgz <15201752137@163.com>
2018-11-04 15:39:13 +0800

    Add epoll support

--- client1.py
+++ client1.py
@@ -2,12 +2,10 @@ import socket
 
 import socket
 HOST = 'localhost'
-PORT = 8005
+PORT = 4343
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-msg = s.recv(1024)
-print (msg)
 count = 0
 while count < 5:
     cmd = input("Please input msg:")
--- epoll_test.py
+++ epoll_test.py
@@ -1,34 +1,112 @@
-import socket, select
+#!/usr/bin/env python
 
-PORT = 8002
+"""Simple server using epoll."""
 
-EOL1 = b'\n\n'
-EOL2 = b'\n\r\n'
-response  = b'HTTP/1.0 200 OK\r\nDate: Mon, 1 Jan 1996 01:01:01 GMT\r\n'
-response += b'Content-Type: text/plain\r\nContent-Length: 13\r\n\r\n'
-response += b'Hello, world!'
-
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-serversocket.bind(('0.0.0.0'), PORT)
-serversocket.listen(4)
-serversocket.setblocking(0)
+from __future__ import print_function
+from contextlib import contextmanager
+import socket
+import select
 
 
-epoll = select.epoll()
-epoll.register(serversocket.fileno(), select.EPOLLIN)
+@contextmanager
+def socketcontext(*args, **kwargs):
+    """Context manager for a socket."""
+    s = socket.socket(*args, **kwargs)
+    try:
+        yield s
+    finally:
+        print("Close socket")
+        s.close()
 
 
-try:
-    connections = {}; requests = {}; response = {}
-    while True:
-        events = epoll.poll(1)
-        for fileno, event in events:
-            if fileno == serversocket.fileno():
-                connection, address = serversocket.accept()
-                connection.setblocking(0)
-                epoll.register(connection.fileno(), select.EPOLLIN)
-finally:
-    epoll.unregister(serversocket.fileno())
-    epoll.close()
-    serversocket.close()
+@contextmanager
+def epollcontext(*args, **kwargs):
+    """Context manager for an epoll loop."""
+    e = select.epoll()
+    e.register(*args, **kwargs)
+    try:
+        yield e
+    finally:
+        print("\nClose epoll loop")
+        e.unregister(args[0])
+        e.close()
+
+
+def init_connection(server, connections, requests, responses, epoll):
+    """Initialize a connection."""
+    connection, address = server.accept()
+    connection.setblocking(0)
+
+    fd = connection.fileno()
+    epoll.register(fd, select.EPOLLIN)
+    connections[fd] = connection
+    requests[fd] = ''
+    responses[fd] = ''
+
+    print ("New server from {0} is ")
+
+def receive_request(fileno, requests, connections, responses, epoll):
+    """Receive a request and add a response to send.
+    Handle client closing the connection.
+    """
+    requests[fileno] += connections[fileno].recv(8).decode('utf-8')
+    print("Received all the request......")
+
+    if requests[fileno] == 'quit' or requests[fileno] == '':
+        print('[{:02d}] exit or hung up'.format(fileno))
+        epoll.unregister(fileno)
+        connections[fileno].close()
+        del connections[fileno], requests[fileno], responses[fileno]
+        return
+
+    else:
+        epoll.modify(fileno, select.EPOLLOUT)
+        msg = requests[fileno][:-1]
+        print("[{:02d}] says: {}".format(fileno, msg))
+        responses[fileno] = 'ACK\n'
+        requests[fileno] = ''
+
+
+def send_response(fileno, connections, responses, epoll):
+    """Send a response to a client."""
+    byteswritten = connections[fileno].send(responses[fileno].encode("utf-8"))
+    responses[fileno] = responses[fileno][byteswritten:]
+    epoll.modify(fileno, select.EPOLLIN)
+
+
+def run_server(socket_options, address):
+    """Run a simple TCP server using epoll."""
+    with socketcontext(*socket_options) as server, \
+            epollcontext(server.fileno(), select.EPOLLIN) as epoll:
+
+        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server.bind(address)
+        server.listen(5)
+        server.setblocking(0)
+        server.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+        print("Listening")
+
+        connections = {}
+        requests = {}
+        responses = {}
+        server_fd = server.fileno()
+
+        while True:
+            events = epoll.poll(1)
+
+            for fileno, event in events:
+                if fileno == server_fd:
+                    init_connection(server, connections, requests, responses, epoll)
+                elif event & select.EPOLLIN:
+                    receive_request(fileno, requests, connections, responses, epoll)
+                elif event & select.EPOLLOUT:
+                    send_response(fileno, connections, responses, epoll)
+
+def handler1(str):
+    return str + ' hello~'
+
+if __name__ == '__main__':
+    try:
+        run_server([socket.AF_INET, socket.SOCK_STREAM], ("0.0.0.0", 4343))
+    except KeyboardInterrupt as e:
+        print("Shutdown")
